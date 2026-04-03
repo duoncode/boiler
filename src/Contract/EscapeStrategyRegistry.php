6d93a3731c9e15b5aa2008ca92e0770b6944b84d
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-04-03 16:15:29 +0200

    Narrow down types in registry interfaces

--- src/Contract/EscapeStrategyRegistry.php
+++ src/Contract/EscapeStrategyRegistry.php
@@ -7,5 +7,6 @@ namespace Duon\Boiler\Contract;
 /** @api */
 interface EscapeStrategyRegistry
 {
+	/** @psalm-assert non-empty-string $name */
 	public function register(string $name, EscapeStrategy $strategy): void;
 }
--- src/Contract/SanitizeStrategyRegistry.php
+++ src/Contract/SanitizeStrategyRegistry.php
@@ -7,5 +7,6 @@ namespace Duon\Boiler\Contract;
 /** @api */
 interface SanitizeStrategyRegistry
 {
+	/** @psalm-assert non-empty-string $name */
 	public function register(string $name, SanitizeStrategy $strategy): void;
 }
